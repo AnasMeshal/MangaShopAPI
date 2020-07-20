c90187bdc64875d57a974cc648b83872a7002a38
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-07-20 18:00:30 +0300

    created a deleting route for the data

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 //Data
-const mangas = require("./mangas");
+let mangas = require("./mangas");
 
 const app = express();
 
@@ -10,6 +10,18 @@ app.use(cors());
 
 app.get("/mangas", (req, res) => {
   res.json(mangas);
+});
+
+app.delete("/mangas/:mangaId", (req, res) => {
+  const { mangaId } = req.params;
+  const foundManga = mangas.find((manga) => manga.id === +mangaId);
+
+  if (foundManga) {
+    mangas = mangas.filter((_manga) => _manga !== foundManga);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "a manga with this ID doesn't exist." });
+  }
 });
 
 app.listen(8000, () => {
