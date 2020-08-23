b0e3c3bdc21a25f04b2ce3b9209e6411e0ee62fb
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-08-23 18:21:08 +0300

    Order and User Relation created

--- db/models/index.js
+++ db/models/index.js
@@ -1,22 +1,25 @@
 const Manga = require("./Manga");
 const Vendor = require("./Vendor");
 const User = require("./User");
+const Order = require("./Order");
 
 User.hasOne(Vendor, { foreignKey: "userId" });
-
 Vendor.belongsTo(User, { as: "user" });
 
 Vendor.hasMany(Manga, {
   foreignKey: { fieldName: "vendorId", allowNull: false },
   as: "mangas",
 });
-
 Manga.belongsTo(Vendor, {
   as: "vendor",
 });
+
+User.hasMany(Order, { as: "orders", foreignKey: "userId" });
+Order.belongsTo(User, { as: "user" });
 
 module.exports = {
   Manga,
   Vendor,
   User,
+  Order,
 };
